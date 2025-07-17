7898b61aea9ac9b3292d62dd88094b2193c12f30
root-kjs <root.kjs82@gmail.com>
2025-07-17 12:27:02 +0900

    main 함수 수정..난독증이 심해진다..

--- src/team0715/AppStart.java
+++ src/team0715/AppStart.java
@@ -4,7 +4,9 @@ import team0715.view.Index;
 
 public class AppStart {
 
- void main(String[] args) {
+    public static void main(String[] args) {
         Index.getInstance().index(); // 도서관리 시스템 : 공통화면( index ) 매소드
     }//main end
 }//class end
+
+
